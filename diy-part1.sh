179d9bb87d7c024df10d5c9c3a340c56dd3e18eb
combat60 <74341349+combat60@users.noreply.github.com>
2022-04-29 02:07:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-passwall' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass.git' >>feeds.conf.default
-echo 'src-git dnsfilter https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-dnsfilter' >>feeds.conf.default
+#echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass.git' >>feeds.conf.default
+#echo 'src-git dnsfilter https://github.com/kenzok8/openwrt-packages/tree/master/luci-app-dnsfilter' >>feeds.conf.default
 
 #echo 'src-git dnsfilter https://github.com/kiddin9/luci-app-dnsfilter' >>feeds.conf.default
 
