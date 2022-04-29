ac05c7460b04b4dece6df7b8f026c285270a791d
combat60 <74341349+combat60@users.noreply.github.com>
2022-04-29 12:57:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,7 +13,7 @@
 
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
